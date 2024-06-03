f8dc78d3c35306e8ba957799f04b7957ac6177c0
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2024-06-02 19:47:39 -0600

    [KAN-1] initialize backend with express server and mongodb connection (#1)

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
