875946d53352b39c981044f4463deb512214c309
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2024-06-06 17:21:39 -0600

    [KAN-3] create user model (#2)

---
+++ server/src/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+const User = mongoose.model('user', UserSchema);
+module.exports = User;
