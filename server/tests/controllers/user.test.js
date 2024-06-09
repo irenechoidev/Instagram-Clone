9fb2d1dfe6a95a7fc32de4bc2bbaebc7f809c016
Irene C <pyrom@MacBook-Air.local>
2024-06-08 18:00:48 -0600

    [KAN-11] implemented unit tests for login API

--- server/tests/controllers/user.test.js
+++ server/tests/controllers/user.test.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 
 const jwt = require('jsonwebtoken');
-const { createUser } = require('../../src/controllers/user');
+const argon2 = require('argon2');
+const { createUser, loginUser } = require('../../src/controllers/user');
 const User = require('../../src/models/user');
 
-test('Test createUser', async () => {
+test('when_Create_User_Success_Username_Encrypted_In_Token', async () => {
   const mockUsername = 'abcd';
   const mockPassword = 'efgh';
   const mockBody = { username: mockUsername, password: mockPassword };
@@ -14,6 +15,25 @@ test('Test createUser', async () => {
   jest.spyOn(User, 'create').mockResolvedValueOnce({});
 
   const payload = await createUser(mockReq, mockRes);
+  const isSuccessful = payload.successful;
+  const token = payload.token;
+  const username = jwt.decode(token).username;
+
+  expect(username).toEqual(mockUsername);
+  expect(isSuccessful).toEqual(true);
+});
+
+test('when_Login_Success_Username_Encrypted_In_Token', async () => {
+  const mockUsername = 'abcd';
+  const mockPassword = 'efgh';
+  const mockBody = { username: mockUsername, password: mockPassword };
+  const mockReq = { body: mockBody };
+  const mockRes = { json: (payload) => payload };
+
+  jest.spyOn(User, 'findOne').mockResolvedValueOnce({});
+  jest.spyOn(argon2, 'verify').mockResolvedValueOnce({});
+
+  const payload = await loginUser(mockReq, mockRes);
   const isSuccessful = payload.successful;
   const token = payload.token;
   const username = jwt.decode(token).username;
