83d4bef596daa6d8f2615fb93eeeb51fb22238b1
Irene C <pyrom@MacBook-Air.local>
2024-06-09 19:40:46 -0600

    [KAN-4] implemented GetUser api

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/', userController.createUser);
 router.post('/login', userController.loginUser);
+router.get('/:username', userController.getUser);
 
 module.exports = router;
