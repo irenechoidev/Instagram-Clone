40d28b5ab7290f0bd815053f43d6b5ce7768912b
Irene C <pyrom@MacBook-Air.local>
2024-06-14 21:25:38 -0600

    [KAN-21] applied http status codes to login user api

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,5 +1,10 @@
 const User = require('../models/user');
 const argon2 = require('argon2');
+const {
+  OK_STATUS_CODE,
+  RESOURCE_NOT_FOUND_STATUS_CODE,
+  UNAUTHORIZED_STATUS_CODE,
+} = require('../commons/constants');
 const { createToken } = require('../utils/createToken');
 
 exports.createUser = async (req, res) => {
@@ -24,16 +29,20 @@ exports.createUser = async (req, res) => {
 exports.loginUser = async (req, res) => {
   const user = await User.findOne({ username: req.body.username });
   if (!user) {
-    return res.json({ successful: false, token: '' });
+    return res
+      .status(RESOURCE_NOT_FOUND_STATUS_CODE)
+      .json({ successful: false, token: '' });
   }
 
   const hashedPassword = user.password;
   const isValid = await argon2.verify(hashedPassword, req.body.password);
   if (!isValid) {
-    return res.json({ successful: false, token: '' });
+    return res
+      .status(UNAUTHORIZED_STATUS_CODE)
+      .json({ successful: false, token: '' });
   }
 
-  return res.json({
+  return res.status(OK_STATUS_CODE).json({
     token: createToken(req.body.username),
     successful: true,
   });
