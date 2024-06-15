9bafcdf4a90a30d9e3ad6800aa13714a2a200ca5
Irene C <pyrom@MacBook-Air.local>
2024-06-14 21:01:21 -0600

    [KAN-25] List Posts API Unit Test

--- server/src/controllers/post.js
+++ server/src/controllers/post.js
@@ -84,12 +84,3 @@ exports.deletePost = async (req, res) => {
     post,
   });
 };
-
-exports.listPosts = async (req, res) => {
-  const { username } = req.params;
-  const posts = await Post.find({ username: username });
-  return res.json({
-    successful: true,
-    posts,
-  });
-};
