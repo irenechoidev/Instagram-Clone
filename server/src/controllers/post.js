e2c6928f4f0d5b5538fe27c93c6c50eadaa2dfe0
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2024-06-27 13:38:26 -0600

    [KAN-33] Update Post API Status Code (#47)

--- server/src/controllers/post.js
+++ server/src/controllers/post.js
@@ -60,12 +60,13 @@ exports.updatePost = async (req, res) => {
 
     post = await Post.findOne({ _id: id });
   } catch (error) {
-    return res.json({
+    return res.status(RESOURCE_NOT_FOUND_STATUS_CODE).json({
       successful: false,
       post,
     });
   }
-  return res.json({
+
+  return res.status(OK_STATUS_CODE).json({
     successful: true,
     post,
   });
