50214f76e012e2e72f358feedeaea7cd6622973a
Irene C <pyrom@MacBook-Air.local>
2024-06-29 11:56:42 -0600

    before each set process.env for unit tests

--- server/src/utils/createToken.js
+++ server/src/utils/createToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 exports.createToken = (username) => {
-  const tokenSecret = process.env.TOKEN_SECRET || '';
+  const tokenSecret = process.env.TOKEN_SECRET;
   const token = jwt.sign({ username }, tokenSecret, { expiresIn: '3d' });
 
   return token;
