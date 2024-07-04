92df1255a1d259711fd29c4c7b99a78e655bdf3c
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2024-07-04 10:24:15 -0600

    [NO-TICKET] revert delete like API change (#70)

--- server/src/controllers/like.js
+++ server/src/controllers/like.js
@@ -1,8 +1,4 @@
-const {
-  BAD_REQUEST,
-  OK_STATUS_CODE,
-  RESOURCE_NOT_FOUND_STATUS_CODE,
-} = require('../commons/constants');
+const { BAD_REQUEST, OK_STATUS_CODE } = require('../commons/constants');
 const Like = require('../models/like');
 
 exports.createLike = async (req, res) => {
@@ -32,22 +28,5 @@ exports.listLikes = async (req, res) => {
   res.status(OK_STATUS_CODE).json({
     successful: true,
     likes,
-  });
-};
-
-exports.deleteLike = async (req, res) => {
-  const { id } = req.params;
-  const like = await Like.findOne({ _id: id });
-
-  if (!like) {
-    return res.status(RESOURCE_NOT_FOUND_STATUS_CODE).json({
-      successful: false,
-      like,
-    });
-  }
-  await Like.deleteOne({ _id: id });
-  return res.status(OK_STATUS_CODE).json({
-    successful: true,
-    like,
   });
 };
