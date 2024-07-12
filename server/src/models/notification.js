8c61c5ee4f2fdd417124cb78cccdda631d038577
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2024-07-12 10:08:20 -0600

    [KAN-66] Notification Model (#93)

---
+++ server/src/models/notification.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const NotificationSchema = new Schema({
+  owner: {
+    type: String,
+    required: true,
+  },
+  sender: {
+    type: String,
+    required: true,
+  },
+  read: {
+    type: Boolean,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+const Notification = mongoose.model('notification', NotificationSchema);
+module.exports = Notification;
