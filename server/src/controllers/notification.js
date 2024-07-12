3530bbbac86ca1d50fe9b2e9fdf1a0f3b31af1ba
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2024-07-12 11:07:01 -0600

    [KAN-67] Create Notification API (#94)

---
+++ server/src/controllers/notification.js
@@ -0,0 +1,25 @@
+const { BAD_REQUEST, OK_STATUS_CODE } = require('../commons/constants');
+const Notification = require('../models/notification');
+
+exports.createNotification = async (req, res) => {
+  let notification = null;
+
+  try {
+    notification = await Notification.create({
+      owner: req.body.owner,
+      sender: req.body.sender,
+      read: req.body.read,
+      createdDate: new Date(),
+    });
+  } catch (error) {
+    return res.status(BAD_REQUEST).json({
+      successful: true,
+      notification,
+    });
+  }
+
+  return res.status(OK_STATUS_CODE).json({
+    successful: true,
+    notification,
+  });
+};
