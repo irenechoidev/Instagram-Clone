92aff8384acebde739de6f32022c549120123564
Irene C <pyrom@MacBook-Air.local>
2024-07-13 15:39:34 -0600

    [NO-TICKET] exposed test API endpoint to clean up test posts

--- server/src/routes/post.js
+++ server/src/routes/post.js
@@ -9,5 +9,6 @@ router.get('/list/:username', postController.listPosts);
 router.get('/:id', postController.getPost);
 router.put('/:id', postController.updatePost);
 router.delete('/:id', postController.deletePost);
+router.delete('/test/clean', postController.cleanTestPosts);
 
 module.exports = router;
