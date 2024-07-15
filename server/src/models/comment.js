33311462b1d2d2e419f511bdf3a354059efa5220
Irene C <pyrom@MacBook-Air.local>
2024-07-15 14:45:30 -0600

    [KAN-95] List Comments API Pagination

--- server/src/models/comment.js
+++ server/src/models/comment.js
@@ -14,6 +14,10 @@ const CommentSchema = new Schema({
     type: String,
     required: true,
   },
+  isTest: {
+    type: Boolean,
+    default: false,
+  },
   createdDate: {
     type: Date,
     required: true,
--- server/src/routes/comment.js
+++ server/src/routes/comment.js
@@ -4,8 +4,10 @@ const commentController = require('../controllers/comment');
 const router = express.Router();
 
 router.post('/', commentController.createComment);
+router.post('/test', commentController.createTestComments);
 router.get('/:postId', commentController.listComments);
 router.put('/:id', commentController.updateComment);
 router.delete('/:id', commentController.deleteComment);
+router.delete('/test/clean', commentController.cleanTestComments);
 
 module.exports = router;
