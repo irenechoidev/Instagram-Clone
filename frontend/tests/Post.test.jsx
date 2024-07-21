fbb80223bb4b10df217cad1ab87380741a88f092
Irene C <pyrom@MacBook-Air.local>
2024-07-20 20:42:10 -0600

    [KAN-123] post component header and photo unit test

---
+++ frontend/tests/Post.test.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Post from '../src/components/Post';
+
+const postProps = {
+  imgURL: 'imgURL',
+  profilePic: 'profilePic',
+  username: 'username',
+};
+
+beforeEach(() => {
+  Enzyme.configure({ adapter: new Adapter() });
+});
+
+describe('<Post />', () => {
+  it('when component mounts provided props are displayed ', () => {
+    const wrapper = mount(<Post {...postProps} />);
+    expect(wrapper.props().username).toEqual(postProps.username);
+    expect(wrapper.props().imgURL).toEqual(postProps.imgURL);
+    expect(wrapper.props().profilePic).toEqual(postProps.profilePic);
+  });
+});
