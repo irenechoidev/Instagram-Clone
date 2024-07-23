489f8670c12cd2df08984ad409ef3e0afc0bb92b
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2024-07-23 13:11:46 -0600

    [KAN-116] Container for ListPosts (#123)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,16 +2,28 @@ import React from 'react';
 import Post from '../components/Post';
 
 const Home = () => {
+  const post = {
+    imgURL: 'test.jpg',
+    profilePic: 'test.jpg',
+    username: 'username',
+    description: 'description description',
+    createdAt: new Date(),
+  };
+
+  const listOfPosts = [post, post, post, post];
+
   return (
-    <>
-      <Post
-        imgURL='test.jpg'
-        profilePic='test.jpg'
-        username='username'
-        description='description description'
-        createdAt={new Date()}
-      />
-    </>
+    <div>
+      {listOfPosts.map((p) => (
+        <Post
+          imgURL={p.imgURL}
+          profilePic={p.profilePic}
+          username={p.username}
+          description={p.description}
+          createdAt={p.createdAt}
+        />
+      ))}
+    </div>
   );
 };
 
