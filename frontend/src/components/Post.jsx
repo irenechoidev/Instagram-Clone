64a136214c376f83131d8b257100b7909cc41aa2
Irene C <pyrom@MacBook-Air.local>
2024-07-24 09:55:02 -0600

    [KAN-132] Apply Conditional Rendering to Likes Preview Section

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import './css/post.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faComment } from '@fortawesome/free-regular-svg-icons';
+import { getCommentsText, getLikesText } from '../utils/formatText';
 
 const Post = ({ imgURL, profilePic, username, description, createdAt }) => {
-  const mockLikesCount = 30;
+  const mockLikesCount = 0;
   const mockCommentCount = 10;
+
+  const likesText = getLikesText(mockLikesCount);
+  const commentsText = getCommentsText(mockCommentCount);
 
   return (
     <div className='post-container'>
@@ -27,18 +31,15 @@ const Post = ({ imgURL, profilePic, username, description, createdAt }) => {
             <FontAwesomeIcon icon={faComment} />
           </div>
         </div>
-        <div className='post-likes'>{mockLikesCount} likes</div>
+
+        {likesText && <div className='post-likes'>{likesText}</div>}
 
         <div className='post-description'>
           <h3>{username}</h3>
           <p>{description}</p>
         </div>
 
-        <div className='post-comment'>
-          {!mockCommentCount && <p>Add a comment...</p>}
-          {mockCommentCount === 1 && <p>View {mockCommentCount} comment</p>}
-          {mockCommentCount > 1 && <p>View all {mockCommentCount} comments</p>}
-        </div>
+        <div className='post-comment'>{commentsText}</div>
 
         <div className='post-create-date'>{createdAt.toDateString()}</div>
       </footer>
