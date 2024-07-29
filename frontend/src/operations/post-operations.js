3565f15e16ec1158616edc13c1f197aee4e53dfa
Irene C <pyrom@MacBook-Air.local>
2024-07-29 13:42:25 -0600

    [KAN-118] Connect to Mock ListPosts API

---
+++ frontend/src/operations/post-operations.js
@@ -0,0 +1,16 @@
+const DEFAULT_LIST_POSTS_LIMIT = 30;
+
+export const listPosts = () => {
+  const posts = [];
+  for (let i = 0; i < DEFAULT_LIST_POSTS_LIMIT; i++) {
+    posts.push({
+      _id: i,
+      imgURL: 'test.jpg',
+      profilePic: 'test.jpg',
+      username: `user${i}`,
+      dexcription: 'description description',
+      createdAt: new Date(),
+    });
+  }
+  return posts;
+};
