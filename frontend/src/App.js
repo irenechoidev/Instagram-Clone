603e1eba7a6fecd19347f0bbec098b054e52a38b
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2024-08-05 19:48:35 -0600

    [KAN-100] Login Page (#142)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import DeadPage from './pages/DeadPage';
 import Likes from './pages/Likes';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/likes/:postId' element={<Likes />} />
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
         <Route path='*' element={<DeadPage />} />
       </Routes>
     </BrowserRouter>
