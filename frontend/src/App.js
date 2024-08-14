c2713291ec2053981aa7f0d46cd58ed721a5003d
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2024-08-14 14:05:58 -0600

    [KAN-105] Upload Post Page (#162)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Followers from './pages/Followers';
 import Following from './pages/Following';
 import Likes from './pages/Likes';
 import Register from './pages/Register';
+import UploadPost from './pages/UploadPost';
 import Login from './pages/Login';
 import PostDetails from './pages/PostDetails';
 import NotificationsPage from './pages/NotificationsPage';
@@ -18,6 +19,7 @@ function App() {
         <Route path='/following/:username' element={<Following />} />
         <Route path='/likes/:postId' element={<Likes />} />
         <Route path='/post/:postId' element={<PostDetails />} />
+        <Route path='/upload' element={<UploadPost />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/' element={<Home />} />
