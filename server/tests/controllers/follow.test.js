56638592c209ff073328d0e127175284def3a836
Irene C <pyrom@MacBook-Air.local>
2024-10-19 20:06:56 -0600

    [KAN-170] Create Follow API Request Start/End Logs

--- server/tests/controllers/follow.test.js
+++ server/tests/controllers/follow.test.js
@@ -139,6 +139,7 @@ const buildMockRequest = () => {
   const mockReq = { metrics: {}, logger: {} };
   mockReq.logger.getLogGroup = jest.fn(() => mockReq.logger);
   mockReq.logger.info = jest.fn();
+  mockReq.logger.error = jest.fn();
 
   mockReq.metrics.createFollowRequestCount = {};
   mockReq.metrics.createFollowLatency = {};
